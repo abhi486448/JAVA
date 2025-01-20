b5b78f81ca5e4b1cab1974d26155c0f9d0e16fc0
abhi486448 <ag096129abhishek@gmail.com>
2025-01-21 03:12:03 +0530

    L2

---
+++ L2/Main.java
@@ -0,0 +1,19 @@
+package L2;
+
+public class Main {
+    public static void main(String[] args) {
+        //output
+        // System.out.println("Hello World");
+        // System.out.println("Hello World");
+        // System.out.print("Hello World");
+
+        // System.out.print("Hello World\n");
+        // System.out.print("Hello World\n");
+        // System.out.print("Hello World");
+
+        System.out.println("*");
+        System.out.println("**");
+        System.out.println("***");
+        System.out.println("****");
+    }
+}
